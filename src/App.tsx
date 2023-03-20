d5d8e11eb8de88cd08cba3f3ea8b252d6deecaeb
Mateusz Kłakus <50427204+Nokiusz@users.noreply.github.com>
2023-03-20 19:27:53 +0100

    chore: swagger generate script (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import React from 'react';
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { DatePicker } from 'antd';
 
 function App() {
   const [count, setCount] = useState(0);
