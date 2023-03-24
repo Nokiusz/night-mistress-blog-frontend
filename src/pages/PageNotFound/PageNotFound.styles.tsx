2e0af23e124ba245774dfdaebbf5963cb4db41cd
Emkerix <michalekrx8@gmail.com>
2023-03-24 20:57:30 +0100

    feat: PageNotFound

--- src/pages/PageNotFound/PageNotFound.styles.tsx
+++ src/pages/PageNotFound/PageNotFound.styles.tsx
@@ -1,1 +1,34 @@
 import styled from 'styled-components';
+import { ThemeProps } from '../../components/theme';
+
+export const Container = styled.div`
+  background-color: ${({ theme }: ThemeProps) => theme.colors.lightgray};
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+export const StatusCode = styled.h1`
+  color: ${({ theme }: ThemeProps) => theme.colors.white};
+  font-size: 198px;
+  font-weight: 600;
+  line-height: 0.7;
+`;
+
+export const Heading = styled.h2`
+  color: ${({ theme }: ThemeProps) => theme.colors.white};
+  font-size: 28px;
+  font-weight: 700;
+  margin: 22px 0px 21px 0px;
+  display: block;
+`;
+
+export const RedParagraph = styled.p`
+  color: ${({ theme }: ThemeProps) => theme.colors.red};
+  display: inline;
+  font-weight: 700;
+`;
--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import tableFlip from '@assets/table-flip.svg';
+import { Container, Heading, RedParagraph, StatusCode } from './PageNotFound.styles';
 
 const PageNotFound = () => {
   return (
-    <div>
+    <Container>
+      <StatusCode>404</StatusCode>
+      <Heading>Sorry, page <RedParagraph>NOT</RedParagraph> found...</Heading>
       <img src={tableFlip} />
-    </div>
+    </Container>
   );
 };
 
