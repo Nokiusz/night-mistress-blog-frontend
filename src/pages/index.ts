7ea57aa157c36b7b32a4b1dc25a172354450fbc6
Mateusz Kłakus <50427204+Nokiusz@users.noreply.github.com>
2023-04-06 21:19:50 +0200

    Feat/basic routing (#8)
    * feat: changed server port to 3000
    
    * feat: basic routing

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,6 @@
+export { default as Login } from './Login/Login';
 export { default as PageNotFound } from './PageNotFound/PageNotFound';
+export { default as Post } from './Post/Post';
+export { default as Posts } from './Posts/Posts';
+export { default as Profile } from './Profile/Profile';
+export { default as Register } from './Register/Register';
