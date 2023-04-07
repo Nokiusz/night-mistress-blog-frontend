c36b47e792270eff82d5be6443af700e45c0de71
Mateusz Kłakus <mati.klakus@gmail.com>
2023-04-08 00:56:28 +0200

    feat: updated hooks

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,16 +1,20 @@
 
 import { useEffect, useState } from 'react';
 import { BASE_URL } from '../constants';
+import { Posts } from '../types';
 
 function usePosts() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Posts>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchPosts() {
       try {
+        setLoading(true);
         const response = await fetch(`${BASE_URL}/Post`);
         const data = await response.json();
         setPosts(data.posts);
+        setLoading(false);
       } catch (error) {
         console.error(error);
       }
@@ -18,7 +22,7 @@ function usePosts() {
     fetchPosts();
   }, []);
 
-  return { posts };
+  return { posts, loading };
 }
 
 export default usePosts;
