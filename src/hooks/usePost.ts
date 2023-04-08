2f239d0055b130bbf5635bda67d7aa10c5ec25f3
Mateusz Kłakus <mati.klakus@gmail.com>
2023-04-08 19:49:11 +0200

    feat: added status code to usePost

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -4,6 +4,7 @@ import { Post } from '../types';
 
 function usePost(friendlyName: string) {
   const [post, setPost] = useState<Post>();
+  const [status, setStatus] = useState<number>();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -11,6 +12,12 @@ function usePost(friendlyName: string) {
       try {
         setLoading(true);
         const response = await fetch(`${BASE_URL}/Post/${friendlyName}`);
+
+        if (!response.ok) {
+          setStatus(response.status);
+          setLoading(false);
+        }
+        
         const data = await response.json();
         setPost(data);
         setLoading(false);
@@ -21,7 +28,7 @@ function usePost(friendlyName: string) {
     fetchPost();
   }, [friendlyName]);
 
-  return { post, loading };
+  return { post, loading, status };
 }
 
 export default usePost;
