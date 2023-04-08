c6426ee629389b3bea557f70c06e5e0ba9acc590
Mateusz Kłakus <mati.klakus@gmail.com>
2023-04-08 19:48:06 +0200

    feat: added Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { AddPost, Login, PageNotFound, Post, Posts, Profile, Register } from './pages';
+import { Layout } from './App.styles';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Posts />}
-        />
-        <Route
-          path="/add/post"
-          element={<AddPost />}
-        />
-        <Route
-          path="/post/:id"
-          element={<Post />}
-        />
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-        <Route
-          path="/profile"
-          element={<Profile />}
-        />
-        <Route
-          path="*"
-          element={<PageNotFound />}
-        />
-      </Routes>
-    </Router>
+    <Layout>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Posts />}
+          />
+          <Route
+            path="/add/post"
+            element={<AddPost />}
+          />
+          <Route
+            path="/post/:slug"
+            element={<Post />}
+          />
+          <Route
+            path="/register"
+            element={<Register />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="/profile"
+            element={<Profile />}
+          />
+          <Route
+            path="*"
+            element={<PageNotFound />}
+          />
+        </Routes>
+      </Router>
+    </Layout>
   );
 }
 
