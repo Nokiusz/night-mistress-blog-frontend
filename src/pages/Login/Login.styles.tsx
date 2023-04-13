9345ca5c0d6105ce2f698bfe2b24cb3baec49bf8
Mateusz Kłakus <mati.klakus@gmail.com>
2023-04-13 23:08:48 +0200

    feat: login form

--- src/pages/Login/Login.styles.tsx
+++ src/pages/Login/Login.styles.tsx
@@ -1,1 +1,97 @@
 import styled from 'styled-components';
+
+import { ThemeProps } from '../../components/theme';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100dvh;
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  background-color: ${({ theme }: ThemeProps) => theme.colors.darkblue};
+  border-radius: 2px;
+  width: 100%;
+  height: 100%;
+  max-width: 450px;
+  max-height: 380px;
+
+  input,
+  .ant-input-affix-wrapper {
+    width: 280px;
+  }
+`;
+
+export const InputWithLabel = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  label {
+    display: flex;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 22px;
+    text-align: center;
+    color: ${({ theme }: ThemeProps) => theme.colors.white};
+  }
+`;
+
+export const Heading = styled.h1`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 22px;
+  margin-bottom: 40px;
+  text-align: center;
+  color: ${({ theme }: ThemeProps) => theme.colors.white};
+`;
+
+interface ButtonLinkProps {
+  theme: ThemeProps;
+  variant: 'primary' | 'secondary';
+}
+
+export const Button = styled.button<ButtonLinkProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  cursor: pointer;
+  padding: 8px 14px;
+  background-color: ${({ theme, variant }) => (variant === 'primary' ? theme.colors.red : 'transparent')};
+  border-radius: 2px;
+  box-shadow: ${({ theme, variant }) =>
+    variant === 'secondary' ? 'inset 0px 0px 0px 2px' + theme.colors.red : 'transparent'};
+  font-weight: 600;
+  font-size: 15px;
+  height: 30px;
+  width: 100%;
+  max-width: 280px;
+  transition: 0.2s ease-out;
+  color: ${({ theme }: ThemeProps) => theme.colors.white};
+
+  :hover {
+    background-color: ${({ theme, variant }) => (variant === 'primary' ? theme.colors.white : 'transparent')};
+    color: ${({ theme, variant }) => (variant === 'primary' ? theme.colors.red : theme.colors.white)};
+    box-shadow: ${({ theme, variant }) =>
+      variant === 'secondary' ? 'inset 0px 0px 0px 2px' + theme.colors.white : 'transparent'};
+  }
+`;
+
+export const Warning = styled.p`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 22px;
+  text-align: center;
+  color: ${({ theme }: ThemeProps) => theme.colors.red};
+`;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,85 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Input, InputRef, notification } from 'antd';
+import { Button, Container, Heading, InputWithLabel, Warning, Wrapper } from './Login.styles';
+import useAuth from '../../hooks/useAuth';
+import { Spinner } from '../../components';
+import { useNavigate } from 'react-router-dom';
+
+type NotificationType = 'success' | 'info' | 'warning' | 'error';
 
 const Login = () => {
-  return <div>Login</div>;
+  const emailRef = useRef<InputRef>(null);
+  const passwordRef = useRef<InputRef>(null);
+  const [warning, setWarning] = useState('');
+  const [loading, setLoading] = useState(false);
+  const { login, loginError } = useAuth();
+  const [api, contextHolder] = notification.useNotification();
+  const navigate = useNavigate();
+
+  const openNotificationWithIcon = (type: NotificationType) => {
+    api[type]({
+      message: 'Login Successful, redirecting to home page...'
+    });
+  };
+
+  const onLogin = async () => {
+    const email = emailRef.current?.input?.value;
+    const password = passwordRef.current?.input?.value;
+    if (!email || !password) {
+      return setWarning('Please fill in all required fields');
+    }
+    setLoading(true);
+
+    const response = await login({ email, password });
+
+    if (loginError === '') {
+      openNotificationWithIcon('success');
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
+    }
+
+    setLoading(false);
+    // console.log({ email, password });
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        {contextHolder}
+        <Heading>Log in</Heading>
+        {loading && <Spinner />}
+        <InputWithLabel>
+          <label htmlFor="name">Email*</label>
+          <Input
+            id="email"
+            name="email"
+            ref={emailRef}
+            placeholder="Email"
+          />
+        </InputWithLabel>
+        <InputWithLabel>
+          <label htmlFor="password">Password*</label>
+          <Input.Password
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+            visibilityToggle
+          />
+        </InputWithLabel>
+        <Warning>{loginError}</Warning>
+        {/* TODO: make a react component out of this styled */}
+        <Button
+          onClick={onLogin}
+          variant="primary"
+        >
+          {loading ? 'Loading..' : 'Log in'}
+        </Button>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Login;
