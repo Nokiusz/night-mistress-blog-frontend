c73f96abdef30cb86acc2ebaefb30dadff46d4cf
Emkerix <michalekrx8@gmail.com>
2023-05-18 11:47:09 +0200

    feat: autocomplete

--- src/components/Navbar/Search/Search.styles.tsx
+++ src/components/Navbar/Search/Search.styles.tsx
@@ -1,42 +1,23 @@
 import styled from 'styled-components';
 import { ThemeProps } from '../../theme';
 
-export const SearchContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: ${({ theme }: ThemeProps) => theme.colors.white};
-`;
+export const Container = styled.div`
+  position: relative;
 
-export const SearchBox = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: ${({ theme }: ThemeProps) => theme.colors.white};
-
-  input {
-    width: 100%;
-    padding: 0px 14px;
-    margin: 0 -1px 0 0;
-    font-size: 15px;
-    height: 38px;
-    font-weight: 600;
-    border-radius: 2px 0 0 2px;
-    color: ${({ theme }: ThemeProps) => theme.colors.grayer};
-    background: ${({ theme }: ThemeProps) => theme.colors.white};
-    box-shadow: inset 0px 0px 0px 2px ${({ theme }: ThemeProps) => theme.colors.gray};
+  .ant-select-selector{
+    border-radius: 4px;
   }
 `;
 
 export const Icon = styled.div`
-  background: ${({ theme }: ThemeProps) => theme.colors.red};
-  padding: 0px 8px;
-  margin: 0 0 0 -1px;
-  height: 38px;
-  border-radius: 0px 2px 2px 0px;
+  position: absolute;
+  background-color: ${({ theme }: ThemeProps) => theme.colors.red};
+  top: 0px;
+  right: 0px;
+  height: 100%;
   display: flex;
-  align-items: center;
   justify-content: center;
-}
+  align-items: center;
+  padding: 0 7px;
+  border-radius: 0 4px 4px 0;
 `;
--- src/components/Navbar/Search/Search.tsx
+++ src/components/Navbar/Search/Search.tsx
@@ -1,18 +1,57 @@
-import React from 'react';
-import magnifyingGlass from '@assets/magnifyingGlassIcon.svg';
-import { Icon, SearchBox, SearchContainer } from './Search.styles';
+import React, { useState } from 'react';
+import usePosts from '../../../hooks/usePosts';
+import { AutoComplete } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { Container, Icon } from './Search.styles';
+
+const { Option } = AutoComplete;
 
 const Search = () => {
+  const { posts } = usePosts();
+
+  const allTags = posts.reduce<string[]>(
+    (tags, post) => [...tags, ...post.tags],
+    []
+  );
+
+  const uniqueTags = [...new Set(allTags)];
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (value: string) => {
+    setInputValue(value);
+  };
+
+  const handleSelect = (value: string) => {
+    console.log('Selected:', value);
+  };
+
+  const filteredTags = uniqueTags.filter((tag) =>
+    tag.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
+  const options = filteredTags.map((tag) => (
+    <Option key={tag} value={tag}>
+      {tag}
+    </Option>
+  ));
+
   return (
-    <SearchContainer>
-      <SearchBox>
-        <input type='search' placeholder='Search by tag'></input>
-        <Icon><img 
-        src={magnifyingGlass} 
-        height={20}
-        /></Icon>
-      </SearchBox>
-    </SearchContainer>
+    <Container>
+      <AutoComplete
+        value={inputValue}
+        onChange={handleInputChange}
+        onSelect={handleSelect}
+        style={{ width: '100%' }}
+        placeholder="Search by tag"
+        dropdownStyle={{ maxHeight: '105px', overflowY: 'auto' }}
+      >
+        {options}
+      </AutoComplete>
+      <Icon>
+        <SearchOutlined/>
+      </Icon> 
+    </Container>
   );
 };
 
